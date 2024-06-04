862cc2ce9847a6863cb7edc2b7f53e5a4c4e2c18
limo-git <Dimpleyadav32@gmail.com>
2024-06-04 20:55:01 +0530

    first commit

---
+++ lib/config.ts
@@ -0,0 +1,17 @@
+const CONFIG = {
+    jsDocs: {
+      apis: ['**/api/**/route.ts'],
+      swaggerDefinition: {
+        info: {
+          title: 'Some Service',
+          version: '0.0.1',
+          description: 'This is an API doc for Some Service.',
+        },
+        
+        
+        
+      },
+    },
+  };
+  
+  export default CONFIG;
---
+++ src/app/docs/page.tsx
@@ -0,0 +1,18 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import SwaggerDoc from '../../../components/swagger-doc';
+import CONFIG from '../../../lib/config';
+
+const getData = async () => {
+  const spec = swaggerJsdoc(CONFIG.jsDocs);
+  console.log(spec)
+
+  return spec;
+};
+
+const Docs = async () => {
+  const data = await getData();
+
+  return <SwaggerDoc spec={data} />;
+};
+
+export default Docs;
